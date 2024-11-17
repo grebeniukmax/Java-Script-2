de8a10805fe6ff9af228cc295b92877974ed5746
гребенюкмакс <148119706+grebeniukmax@users.noreply.github.com>
2024-11-17 20:43:52 +0200

    Java-Script-2

---
+++ code.js
@@ -0,0 +1,53 @@
+"use strict"
+// Перше //
+const farengeyt = 15 ;
+const celsius = ( farengeyt * 1/8 ) + 32 ;
+console.log(celsius) ;
+
+// Друге //
+const daysInMonth = 30 ;
+const hoursInMonth = daysInMonth * 24 ;
+const minutesInMonth = daysInMonth * 60 ;
+console.log(hoursInMonth) ;
+console.log(minutesInMonth) ;
+
+// Трете //
+let energyOfPlayer = 100 ;
+let helthOfPlayer = 100 ;
+let minusEnergy = (energyOfPlayer - 76);
+let minusHelth = (helthOfPlayer - 52);
+console.log(minusEnergy);
+console.log(minusHelth);
+
+// Четверте //
+const summa = 540;
+const summaWithSale = (summa / 10 * 100);
+const marketSumma = (summa - summaWithSale);
+console.log(marketSumma);
+
+// П'яте //
+let numberWithComma = 9.99;
+let numberWithMath = Math.floor(numberWithComma);
+console.log(numberWithMath);
+
+// Шосте //
+let numberWithCommaTwo = 3.89;
+let numberWithParse = parseFloat(numberWithCommaTwo);
+console.log(numberWithParse);
+
+// Сьоме //
+const seventhNumber = "23";
+const seventhNumberInt = parseInt(seventhNumber);
+console.log(seventhNumberInt);
+// Восьме //
+let squareRoot = 2; //! Спеціально знайшов в інеті як буе квадратний корінь по англійськи //
+let squareRootTwo = Math.sqrt (squareRoot);
+console.log(squareRootTwo)
+// Дев'яте //
+const stringNumber = "666";
+const normalNumber = parseInt(stringNumber);
+console.log(normalNumber);
+
+const numberNumber = "100";
+const numberNumberNumber = toString(numberNumber);
+console.log(numberNumberNumber);
